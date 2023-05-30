b479fb1f2c2b2add3c29a5ebceb9a97ccbf663df
Lucas Rodrigues de Oliveira <lucasroliveira98@gmail.com>
2023-05-29 22:27:45 -0300

    user e2e tests

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -13,6 +13,7 @@ import { LoginDto } from '../src/modules/auth/dto/login.dto';
 import { UserService } from '../src/modules/user/user.service';
 import { UserResult } from '../src/modules/user/dto/user-result.dto';
 import { UserTypeEnum } from '../src/modules/user-type/type/user-type.enum';
+import { UpdateUserDto } from 'src/modules/user/dto/update-user.dto';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
@@ -21,6 +22,8 @@ describe('UserController (e2e)', () => {
     hashPassword: jest.fn((password) => password),
     find: jest.fn(),
     list: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
   };
 
   beforeAll(async () => {
@@ -45,6 +48,12 @@ describe('UserController (e2e)', () => {
   });
 
   describe('/', () => {
+    it(`GET - ${HttpStatus.UNAUTHORIZED}`, () => {
+      return request(app.getHttpServer())
+        .get('/user')
+        .expect(HttpStatus.UNAUTHORIZED);
+    });
+
     const user: Partial<User> = {
       id: '0',
       password: '12345',
@@ -111,6 +120,186 @@ describe('UserController (e2e)', () => {
           .get('/user')
           .set('authorization', token)
           .expect(HttpStatus.FORBIDDEN);
+      });
+    });
+  });
+
+  describe('/:userId', () => {
+    describe('GET', () => {
+      const user: Partial<User> = {
+        id: '0',
+        password: '12345',
+      };
+
+      const userResult: UserResult = {
+        id: '0',
+        type: UserTypeEnum.DEFAULT,
+        name: 'test',
+        username: 'test.test',
+      };
+
+      const loginDto: LoginDto = {
+        username: 'test',
+        password: '12345',
+        remember: false,
+      };
+
+      let token: string;
+
+      beforeEach(async () => {
+        userService.findByUsername.mockResolvedValueOnce(user);
+        userService.find.mockResolvedValue(userResult);
+
+        const { body } = await request(app.getHttpServer())
+          .post('/auth/login')
+          .send(loginDto);
+
+        token = `Bearer ${body.token}`;
+      });
+
+      it(`${HttpStatus.UNAUTHORIZED}`, () => {
+        return request(app.getHttpServer())
+          .get(`/user/${user.id}`)
+          .expect(HttpStatus.UNAUTHORIZED);
+      });
+
+      it(`${HttpStatus.OK}`, () => {
+        return request(app.getHttpServer())
+          .get(`/user/${user.id}`)
+          .set('authorization', token)
+          .expect(HttpStatus.OK)
+          .then((response) => {
+            expect(response.body).toStrictEqual(userResult);
+          });
+      });
+    });
+
+    describe('PUT', () => {
+      const user: Partial<User> = {
+        id: '0',
+        password: '12345',
+      };
+
+      const userResult: UserResult = {
+        id: '0',
+        type: UserTypeEnum.DEFAULT,
+        name: 'test',
+        username: 'test.test',
+      };
+
+      const loginDto: LoginDto = {
+        username: 'test',
+        password: '12345',
+        remember: false,
+      };
+
+      const updateUserDto: UpdateUserDto = {
+        name: 'updated test',
+      };
+
+      let token: string;
+
+      beforeEach(async () => {
+        userService.findByUsername.mockResolvedValueOnce(user);
+        userService.find.mockResolvedValue(userResult);
+        userService.update.mockResolvedValueOnce(user);
+
+        const { body } = await request(app.getHttpServer())
+          .post('/auth/login')
+          .send(loginDto);
+
+        token = `Bearer ${body.token}`;
+      });
+
+      it(`${HttpStatus.UNAUTHORIZED}`, () => {
+        return request(app.getHttpServer())
+          .put(`/user/${user.id}`)
+          .expect(HttpStatus.UNAUTHORIZED);
+      });
+
+      it(`${HttpStatus.OK}`, () => {
+        return request(app.getHttpServer())
+          .put(`/user/${user.id}`)
+          .set('authorization', token)
+          .send(updateUserDto)
+          .expect(HttpStatus.OK)
+          .then((response) => {
+            expect(response.body).toStrictEqual(user);
+          });
+      });
+    });
+
+    describe('DELETE', () => {
+      const user: Partial<User> = {
+        id: '0',
+        password: '12345',
+      };
+
+      const userResult: UserResult = {
+        id: '0',
+        type: UserTypeEnum.ADMIN,
+        name: 'test',
+        username: 'test.test',
+      };
+
+      const loginDto: LoginDto = {
+        username: 'test',
+        password: '12345',
+        remember: false,
+      };
+
+      let token: string;
+
+      it(`${HttpStatus.UNAUTHORIZED}`, () => {
+        return request(app.getHttpServer())
+          .delete(`/user/${user.id}`)
+          .expect(HttpStatus.UNAUTHORIZED);
+      });
+
+      describe('ADMIN', () => {
+        beforeEach(async () => {
+          userService.findByUsername.mockResolvedValueOnce(user);
+          userService.find.mockResolvedValue(userResult);
+          userService.delete.mockResolvedValueOnce(true);
+
+          const { body } = await request(app.getHttpServer())
+            .post('/auth/login')
+            .send(loginDto);
+
+          token = `Bearer ${body.token}`;
+        });
+
+        it(`${HttpStatus.OK}`, () => {
+          return request(app.getHttpServer())
+            .delete(`/user/${user.id}`)
+            .set('authorization', token)
+            .expect(HttpStatus.OK)
+            .then((response) => {
+              expect(JSON.parse(response.text)).toBe(true);
+            });
+        });
+      });
+
+      describe('DEFAULT', () => {
+        beforeEach(async () => {
+          userResult.type = UserTypeEnum.DEFAULT;
+          userService.findByUsername.mockResolvedValueOnce(user);
+          userService.find.mockResolvedValue(userResult);
+          userService.delete.mockResolvedValueOnce(true);
+
+          const { body } = await request(app.getHttpServer())
+            .post('/auth/login')
+            .send(loginDto);
+
+          token = `Bearer ${body.token}`;
+        });
+
+        it(`${HttpStatus.FORBIDDEN}`, () => {
+          return request(app.getHttpServer())
+            .delete(`/user/${user.id}`)
+            .set('authorization', token)
+            .expect(HttpStatus.FORBIDDEN);
+        });
       });
     });
   });
